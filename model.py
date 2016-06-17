f4f9f5d663faf7d35a997099ccf4bc3838ded01c
Owen Campbell <owen.campbell@empiria.co.uk>
2016-06-17 19:12:21 +0100

    Tidy up script

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import yaml
 from pathlib import Path
-import matplotlib as plt
 
 file = Path('omni4d.core.yaml').open('r')
 model = yaml.load(file)
@@ -18,6 +17,4 @@ for sign, attributes in model.items():
             graph.add_edge(sign, object, role=details['role'])
 
 
-# print(graph.nodes(data=True))
-# print(graph.edges(data=True))
 nx.write_graphml(graph, 'omni4d.core.graphml')
