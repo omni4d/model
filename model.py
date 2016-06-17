de316aa3bb8ba052e8edb15951a649b63c43108f
Owen Campbell <owen.campbell@empiria.co.uk>
2016-06-17 19:21:46 +0100

    Tidy up model.py

--- model.py
+++ model.py
@@ -2,17 +2,19 @@ import networkx as nx
 import yaml
 from pathlib import Path
 
+tuple_types = [
+    'whole_part_tuple',
+    'class_member_tuple',
+    'ordinary_tuple'
+]
+
 file = Path('omni4d.core.yaml').open('r')
 model = yaml.load(file)
 graph = nx.Graph()
 
 for sign, attributes in model.items():
     graph.add_node(sign, type=attributes['type'])
-    if attributes['type'] in [
-        'whole_part_tuple',
-        'class_member_tuple',
-        'ordinary_tuple'
-    ]:
+    if attributes['type'] in tuple_types:
         for object, details in attributes['objects'].items():
             graph.add_edge(sign, object, role=details['role'])
 
